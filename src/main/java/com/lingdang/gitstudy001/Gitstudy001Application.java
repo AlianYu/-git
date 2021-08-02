867625f45922b63ee350a8d66140c172cd365d3f
lingdang <ylq1349578685@163.com>
2021-08-02 16:32:08 +0800

    第一次提交

---
+++ src/main/java/com/lingdang/gitstudy001/Gitstudy001Application.java
@@ -0,0 +1,13 @@
+package com.lingdang.gitstudy001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitstudy001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitstudy001Application.class, args);
+    }
+
+}
